5977a562d44cfd094e43da47c572e8f2aab46a2a
sgonzo1976 <78392188+sgonzo1976@users.noreply.github.com>
2021-03-27 21:34:01 -0500

    Added live data for time

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    let day = days[date.getDay()];
+    return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${hours}:${minutes}`;
+}
+
 function showWeatherInformation(response) {
+    let dateElement = document.querySelector("#current-day-and-time");
     let currentTemperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description-of-current-condition");
     let windElement = document.querySelector("#wind");
     let humidityElement = document.querySelector("#humidity");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
     currentTemperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
