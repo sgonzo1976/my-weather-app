a81ef0a4cd5b44bbac88b72e79599918bce531bc
sgonzo1976 <78392188+sgonzo1976@users.noreply.github.com>
2021-03-27 22:19:17 -0500

    added weather icon from API

--- src/index.js
+++ src/index.js
@@ -31,12 +31,19 @@ function formatHours(timestamp) {
 
 function showWeatherInformation(response) {
     let dateElement = document.querySelector("#current-day-and-time");
+    let iconElement = document.querySelector("#weather-icon");
     let currentTemperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description-of-current-condition");
     let windElement = document.querySelector("#wind");
     let humidityElement = document.querySelector("#humidity");
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+    
     currentTemperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
